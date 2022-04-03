cca2afd627a0dd42088dcdaaea4a330bde0a8cff
Co PhamMinh <cominhphamhcb@gmail.com>
2022-04-03 21:21:02 +0700

    search-bar

--- CellPhoneX/Controllers/ProductController.cs
+++ CellPhoneX/Controllers/ProductController.cs
@@ -9,10 +9,37 @@ namespace CellPhoneX.Controllers
 {
     public class ProductController : Controller
     {
+        CellPhoneDBDataContext data = new CellPhoneDBDataContext();
         // GET: Product
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpPost]
+        public JsonResult SearchPhone(string keyword)
+        {
+            var listResult = data.products.Where(p => p.product_name.Contains(keyword.ToLower())).ToList();
+            var result = new List<product_version>();
+            if (listResult == null)
+            {
+                return Json(result);
+            }
+            else
+            {
+                listResult = listResult.OrderByDescending(p => p.release_date).Take(5).ToList();
+                foreach (var item in listResult)
+                {
+                    List<product_version> listPhone_version = data.product_versions.Where(p => p.product_id == item.product_id)
+                                                                                   .OrderByDescending(p => p.amount)
+                                                                                   .Take(2).ToList();
+                    product_version resultPhone_version = listPhone_version.First();
+                    result.Add(resultPhone_version);
+                }
+            }
+            return Json(result);
+        }
+
     }
 }
